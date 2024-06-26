13014b48bdf02876c17f6b917ba9e4dc977851eb
kjserg <kjserg@i.ua>
2024-06-26 11:04:43 +0300

    fix pending project next  comments

--- wp-content/themes/wmcrm/functions/helpers.php
+++ wp-content/themes/wmcrm/functions/helpers.php
@@ -74,7 +74,12 @@ function get_comments_next_link( $max_page, $link ) {
 	$paged    = $_GET['pagenumber'] ?? 1;
 	$nextpage = intval( $paged ) + 1;
 	if ( $nextpage <= $max_page ) {
-		return ' <a class="circle-button next-post-link" href="' . $url . '?pagenumber=' . $nextpage . '"  ><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+		$mystring = $url;
+		$findme   = '?';
+		$pos      = strpos( $mystring, $findme );
+		$sep      = $pos === false ? '?' : '&';
+
+		return ' <a class="circle-button next-post-link" href="' . $url . $sep . 'pagenumber=' . $nextpage . '"  ><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M9 0H7V7H0V9H7V16H9V9H16V7H9V0Z" fill="#C9CBE4"></path>
                                 </svg></a>';
 	}
