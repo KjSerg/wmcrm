f898e6f5dfe427f08e9730e8b6f86f2f9efb847c
Сергій <kjserg@i.ua>
2025-02-17 11:24:40 +0200

    підправив верстку

--- wp-content/themes/wmcrm/functions.php
+++ wp-content/themes/wmcrm/functions.php
@@ -13,9 +13,9 @@ function wmcrm_scripts() {
 
 	wp_enqueue_style( 'wmcrm-jquery-ui', 'https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css', array(), '1.0' );
 
-	wp_enqueue_style( 'wmcrm-app', get_template_directory_uri() . '/assets/css/app.css', array(), '1.0.2.6' );
+	wp_enqueue_style( 'wmcrm-app', get_template_directory_uri() . '/assets/css/app.css', array(), '1.0.2.8' );
 
-	wp_enqueue_script( 'wmcrm-app-scripts', get_template_directory_uri() . '/assets/js/app.js', array(), '1.0.6.4', true );
+	wp_enqueue_script( 'wmcrm-app-scripts', get_template_directory_uri() . '/assets/js/app.js', array(), '1.0.6.5', true );
 
 	wp_localize_script( 'ajax-script', 'AJAX', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 }
